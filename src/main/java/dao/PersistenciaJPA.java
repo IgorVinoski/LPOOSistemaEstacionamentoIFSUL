9b4cbcb9f7ac9f91c9592876d829db2461173343
Igor Vinoski <igor.vinoski2@gmail.com>
2024-10-22 11:05:48 -0300

    all files

---
+++ src/main/java/dao/PersistenciaJPA.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author igor
+ */
+public class PersistenciaJPA implements InterfaceDB {
+     EntityManager entity;
+    EntityManagerFactory factory;
+    
+    public PersistenciaJPA(){
+        factory = Persistence.createEntityManagerFactory("pu_lpoo_estacionamento");
+        
+        entity = factory.createEntityManager();
+    }
+
+    @Override
+    public Boolean conexaoAberta() {
+        return entity.isOpen();
+        
+    }
+
+    @Override
+    public void fecharConexao() {
+        entity.close();
+    }
+
+    @Override
+    public Object find(Class c, Object id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void persist(Object o) throws Exception {
+        entity = getEntityManager();
+        entity.getTransaction().begin();
+        entity.persist(o);
+        entity.getTransaction().commit();
+    }
+
+    @Override
+    public void remover(Object o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    public EntityManager getEntityManager(){
+        if(entity == null || entity.isOpen()){
+            entity = factory.createEntityManager();
+        }
+        return entity;
+    }
+}
--- src/main/java/model/Modelo.java
+++ src/main/java/model/Modelo.java
@@ -4,12 +4,41 @@
  */
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  *
  * @author igor
  */
+@Entity
 public class Modelo {
     private String descricao;
+    @Id
     private int id;
     private Marca marca;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
 }
